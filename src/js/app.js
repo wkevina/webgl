638d3df0dfce28ab7d4df841d560d81c68f58658
Kevin Ward <antiomiae@gmail.com>
2017-10-02 09:00:11 -0700

    Refactor startup code into App class

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,36 @@
-import {
-    programFromPaths
-} from './shader-util.js';
+import {createProgram} from './shader-util.js';
 
 import * as glMatrix from 'gl-matrix';
 
 class App {
-    constructor(opts) {
+    constructor({el}) {
+        if (typeof canvas === 'string') {
+            this.canvas = document.getElementById(el);            
+        } else {
+            this.canvas = el;
+        }    
         
+        // create rendering context        
+        this.gl = this.canvas.getContext('webgl2');
+    }
+    
+    start() {
+        console.log('start');
+
+        this.canvas.width = 320;
+        this.canvas.height = 240;
+
+        this.run();
+    }
+
+    async run() {
+        this.gl.viewport(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+        // Load default shader
+        let defaultShader = await createProgram(this.gl, 'shaders/default.glsl.vert', 'shaders/default.glsl.frag');
     }
 }
 
-function start() {
-    console.log('start');
-    const mountPoint = document.getElementById('content');
-    const canvas = document.createElement('canvas');
-    mountPoint.appendChild(canvas);
-    
-    canvas.width = 320;
-    canvas.height = 240;
-
-    // create rendering context
-    const gl = canvas.getContext('webgl2');
-
-    run(canvas, gl);
-}
-
-async function run(canvas, gl) {
-    gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // Load default shader
-    let defaultShader = await programFromPaths(
-        gl,
-        'shaders/default.glsl.vert',
-        'shaders/default.glsl.frag'
-    );    
-}
-
-//start();
+export {App};
